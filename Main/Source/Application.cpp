4a8891b694f22bfe90c1b4aa4b9e7b582ad08a45
iivatrak <daniiiiila123@gmail.com>
2026-01-13 22:12:41 +0300

    Created particle generator.

--- Main/Source/Application.cpp
+++ Main/Source/Application.cpp
@@ -3,8 +3,13 @@
 #include "imgui.h"
 #include "imgui-SFML.h"
 
+#include <fstream>
+
 Application::Application()
-: m_Window(sf::VideoMode(sf::Vector2u(800, 600)), "Prac1") {
+: m_Window(sf::VideoMode(sf::Vector2u(800, 600)), "Prac1")
+, m_RandomGenerator(m_RandomDevice())
+, m_NormalFDistribution(0.0f, 1.0f)
+, m_UIntDistribution(0, 255) {
     m_Window.setFramerateLimit(60);
 }
 
@@ -34,12 +39,69 @@ void Application::processEvents() {
 }
 
 void Application::guiRender() {
-    ImGui::ShowDemoWindow();
+    if (ImGui::Begin("Panel")) {
+        ImGui::SeparatorText("Rectangle");
+        if (ImGui::Button("Spawn rectangle")) {
+            spawnRectangle();
+        }
+        if (ImGui::Button("Clear rectangles")) {
+            clearRectangles();
+        }
+        ImGui::SeparatorText("File");
+        if (ImGui::Button("Save")) {
+            saveRectangles();
+        }
+        if (ImGui::Button("Load last")) {
+            loadRectangles();
+        }
+    }
+    ImGui::End();
+}
+
+void Application::renderRectangles() {
+    for (const auto& rectangle : m_Rectangles) {
+        m_Window.draw(rectangle);
+    }
 }
 
 void Application::render() {
     guiRender();
     m_Window.clear();
+    renderRectangles();
     ImGui::SFML::Render(m_Window);
     m_Window.display();
+}
+
+void Application::spawnRectangle() {
+    const sf::View& view = m_Window.getView();
+    sf::Vector2f position = { 1.f, 1.f };
+    position.x *= m_NormalFDistribution(m_RandomGenerator);
+    position.y *= m_NormalFDistribution(m_RandomGenerator);
+    const sf::Vector2f viewSize = view.getSize();
+    const sf::Vector2f viewTolLeft = view.getCenter() - viewSize / 2.f;
+    position.x *= viewSize.x;
+    position.y *= viewSize.y;
+    position += viewTolLeft;
+    sf::Color color;
+    color.r = m_UIntDistribution(m_RandomGenerator);
+    color.g = m_UIntDistribution(m_RandomGenerator);
+    color.b = m_UIntDistribution(m_RandomGenerator);
+    sf::RectangleShape rectangle;
+    rectangle.setSize({ 50.f, 50.f });
+    rectangle.setPosition(position);
+    rectangle.setFillColor(color);
+    m_Rectangles.push_back(rectangle);
+}
+
+void Application::clearRectangles() {
+    m_Rectangles.clear();
+}
+
+void Application::saveRectangles() {
+    std::ofstream file;
+    file.open("rectangles.dat", std::ios::binary |  std::ios::trunc);
+}
+
+void Application::loadRectangles() {
+
 }
--- Main/Source/Application.hpp
+++ Main/Source/Application.hpp
@@ -1,8 +1,11 @@
 #ifndef APPLICATION_HPP
 #define APPLICATION_HPP
 
+#include <SFML/Graphics/RectangleShape.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/System/Clock.hpp>
+#include <vector>
+#include <random>
 
 class Application {
 public:
@@ -15,11 +18,23 @@ public:
 private:
     void processEvents();
     void guiRender();
+    void renderRectangles();
     void render();
+
+    void spawnRectangle();
+    void clearRectangles();
+
+    void saveRectangles();
+    void loadRectangles();
 
 private:
     sf::RenderWindow m_Window;
     sf::Clock m_Clock;
+    std::random_device m_RandomDevice;
+    std::mt19937 m_RandomGenerator;
+    std::uniform_real_distribution<float> m_NormalFDistribution;
+    std::uniform_int_distribution<unsigned int> m_UIntDistribution;
+    std::vector<sf::RectangleShape> m_Rectangles;
 };
 
 #endif // APPLICATION_HPP
